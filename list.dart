93ec3c1216bc2f83e006214925dabffd05adabb0
Prabhu <bosesenthilprabhu@gmail.com>
2023-07-19 07:15:55 +0530

    different way of handling state of listwidget

--- list.dart
+++ list.dart
@@ -1,42 +1,55 @@
 import 'package:flutter/material.dart';
 
-class ListWidget extends StatelessWidget {
-  const ListWidget({super.key});
+class ListWidget extends StatefulWidget {
+  const ListWidget({Key? key}) : super(key: key);
+
+  @override
+  _ListWidgetState createState() => _ListWidgetState();
+}
+
+class _ListWidgetState extends State<ListWidget> {
+  List<int> listCount = List<int>.generate(100, (index) => 0);
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> list = List.generate(100, (i) => const ListItemWidget());
-    return ListView.builder(itemCount: list.length,itemBuilder: (context,index)=> list[index]);
+    return ListView.builder(
+      itemCount: listCount.length,
+      itemBuilder: (context, index) {
+        return ListItemWidget(
+          count: listCount[index],
+          onCountIncremented: () => setState(() => listCount[index]++),
+        );
+      },
+    );
   }
 }
 
-class ListItemWidget extends StatefulWidget {
-  const ListItemWidget({super.key});
-  @override
-  State<ListItemWidget> createState() => _ListItemWidgetState();
-}
+class ListItemWidget extends StatelessWidget {
+  final int count;
+  final VoidCallback onCountIncremented;
 
+  const ListItemWidget({
+    Key? key,
+    required this.count,
+    required this.onCountIncremented,
+  }) : super(key: key);
 
-class _ListItemWidgetState extends State<ListItemWidget> with AutomaticKeepAliveClientMixin {
-  int _count = 0;
   @override
   Widget build(BuildContext context) {
-    super.build(context);
     return Container(
-        color: Colors.white,
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("$_count"),
-            IconButton(
-              icon:  const Icon(Icons.add),
-              highlightColor: Colors.black,
-              onPressed: (){ setState(() => _count++);},
-            ),
-          ],
-        ));
+      color: Colors.white,
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(count.toString()),
+          IconButton(
+            icon: const Icon(Icons.add),
+            highlightColor: Colors.black,
+            onPressed: onCountIncremented,
+          ),
+        ],
+      ),
+    );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
